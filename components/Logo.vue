5042c06a7dbccacaf117c370c455738d6aa1b382
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2019-11-13 09:48:06 +0100

    add nnuxtlink to home

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="">
-    <img src="~/assets/example-logo.svg" alt="" />
+    <nuxt-link to="/">
+      <img class="inline" src="~/assets/example-logo.svg" alt="" />
+    </nuxt-link>
   </div>
 </template>
 
